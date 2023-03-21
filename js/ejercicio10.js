cd0a2f779dadb7b9de3cdd705f56eef73f1398a3
Julian <julianser@hotmail.com.ar>
2023-03-21 17:28:43 -0300

    Se comenzo el ejercicio10

---
+++ js/ejercicio10.js
@@ -0,0 +1,95 @@
+//10- crear una clase aeropuerto con las propiedades nombreAeropuerto y lista de aviones, esta clase deberá contener el método agregarAvion, el cual recibirá un objeto de tipo Avión, además de un método buscarAvion el cual recibirá el nombre de un avión y devolverá información en caso de encontrarlo, si no lo encontró indicar con un mensaje.
+//Por su parte los aviones tendrán las propiedades: nombre, capacidad, destino, lista de pasajeros. Los aviones tienen el método abordar el cual permite que un pasajero suba al avión solo si hay capacidad disponible en el mismo, caso contrario mostrar un mensaje que indique que el avión está lleno.
+
+//Crear un objeto de tipo aeropuerto llamado "Aeropuerto Internacional", crear 3 objetos aviones a diferentes destinos. Agregar los 3 aviones al aeropuerto, buscar un avión y usar el método abordar.
+
+class Aeropuerto{
+    #nombreAeropuerto
+    constructor(nombreAeropuerto,listaAviones)
+    {
+        this.#nombreAeropuerto=nombreAeropuerto;
+        this.listaAviones=[]
+    }
+    
+    get nombreAeropuerto()
+    {
+        return this.#nombreAeropuerto
+    }
+
+    set nombreAeropuerto(newnombreAeropuerto)
+    {
+        this.#nombreAeropuerto = newnombreAeropuerto;
+    }
+
+    agregarAvion(avion)
+    {
+        this.listaAviones.push(avion);
+    }
+
+    buscarAvion(avion)
+    {
+        if(avion)
+        {
+            document.write(`${this}`)
+        }
+    }
+}
+
+class Avion extends Aeropuerto{
+    #nombreAvion
+    #destino
+    #listaPasajeros
+    constructor(nombreAvion,capacidad,destino,listaPasajeros)
+    {
+        this.#nombreAvion=nombreAvion;
+        this.capacidad=capacidad;
+        this.#destino=destino;
+        this.#listaPasajeros=listaPasajeros
+    }
+
+    get nombreAvion()
+    {
+        return this.#nombreAvion
+    }
+
+    set nombreAvion(newNombreAvion)
+    {
+        this.#nombreAvion=newNombreAvion
+    }
+
+    getCapacidad()
+    {
+        return this.capacidad;
+    }
+    setCapacidad(newCapacidad)
+    {
+        this.capacidad=newCapacidad;
+    }
+    get destino()
+    {
+        return this.#destino;
+    }
+
+    set destino(newDestino)
+    {
+        this.#destino=newDestino;
+    }
+
+    get listaPasajeros()
+    {
+        return this.#listaPasajeros
+    }
+
+    set listaPasajeros(newListaPasajeros)
+    {
+        this.#listaPasajeros=newListaPasajeros
+    }
+
+    abordar(cantidad){
+        if(cantidad <= this.capacidad)
+        {
+            
+        }
+    }
+
+}
